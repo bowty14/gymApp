1b2ad930c53c4cfab7910780787b07a028acddb5
Reid Ashwill <reidashwill@gmail.com>
2020-10-23 11:21:51 -0700

    reinitialize app and add firebase

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Firebase from './firebase'
 
 export default function App() {
+
+  console.log(Firebase.auth())
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
